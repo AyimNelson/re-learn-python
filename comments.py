0e3a35657c01a4ed597990cbf4a25940ee98cef4
AyimNelson <ayimnelson01@gmail.com>
2023-08-20 18:28:41 +0000

    Learnt about comments in python

---
+++ comments.py
@@ -0,0 +1,12 @@
+# This is a single line comment
+
+"""
+A multiline comment
+
+"""
+"""
+A comment is a programmer-readable explanation or annotation in the source code of a computer program. 
+ 
+They are added with the purpose of making the source code easier for humans to understand, and are generally ignored by compilers and interpreters.
+
+"""
